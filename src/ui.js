7409140266708c60cb97e91d525428678e0adab5
Malinna Leach <malinnab@yahoo.com>
2016-10-27 15:54:45 +0100

    some ajax functionality

--- src/ui.js
+++ src/ui.js
@@ -1,13 +1,26 @@
 $( document ).ready(function() {
   myThermo = new Thermostat();
+  var weather = "";
 
   var update = function() {
   $('#temperature').html(myThermo.temperature);
   $('#power-saving-mode').html(myThermo.psm);
   $('#status').html(myThermo.status());
+  $('#weather').html(weather);
 };
 
   update();
+
+    $.ajax({
+      type:'GET',
+      url:"http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c2acf68f8c62606b5b06c3c51e245f19",
+      data:"format=json",
+      success:function(feed) {
+          document.write(feed);
+          weather = JSON.stringify(feed);
+      },
+      dataType:'jsonp'
+    });
 
   $( "#up" ).click(function( event ) {
       myThermo.up();
