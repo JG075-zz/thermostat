583e5a83f5064b58234d629e5c8294d6d46c4b83
James Gardiner <jamesgardiner075@gmail.com>
2016-10-27 10:48:14 +0100

    Add 'up' method

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -5,7 +5,12 @@ describe("Thermostat", function() {
     myThermo = new Thermostat();
   });
 
-  it("should be start at 20 degrees", function() {
+  it("should start at 20 degrees", function() {
     expect(myThermo.temperature).toEqual(20);
+  });
+
+  it("allows temperature to be increased", function() {
+    myThermo.up();
+    expect(myThermo.temperature).toEqual(21);
   });
 });
