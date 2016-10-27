98b34e4c20e52b598b6c559ba3481c8bd2f1a667
Malinna Leach <malinnab@yahoo.com>
2016-10-27 10:57:37 +0100

    add min temp of 10

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -13,4 +13,15 @@ describe("Thermostat", function() {
     myThermo.up();
     expect(myThermo.temperature).toEqual(21);
   });
+
+  it("allows temperature to be decreased", function() {
+    myThermo.down();
+    expect(myThermo.temperature).toEqual(19);
+  });
+
+  it("will not allow temperature to go below minimum", function() {
+    myThermo.temperature = myThermo.minTemp;
+    myThermo.down();
+    expect(myThermo.temperature).toEqual(myThermo.minTemp);
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,7 +1,14 @@
 function Thermostat() {
   this.temperature = 20;
+  this.minTemp = 10;
 }
 
 Thermostat.prototype.up = function() {
   this.temperature += 1;
+};
+
+Thermostat.prototype.down = function() {
+  if (this.temperature > this.minTemp){
+    this.temperature -= 1;
+  };
 };
